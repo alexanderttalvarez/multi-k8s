46f7b251d2ea9ffcee8cb3feefe6327073139db9
alexanderttalvarez <aelxan@gmail.com>
2020-07-12 14:23:44 +0200

    Change username in deploy file

--- deploy.sh
+++ deploy.sh
@@ -1,15 +1,15 @@
-docker build -t alexanderttalvarez/multi-client:latest -t alexanderttalvarez/multi-client:$SHA -f ./client/Dockerfile ./client
-docker build -t alexanderttalvarez/multi-server:latest -t alexanderttalvarez/multi-server:$SHA -f ./server/Dockerfile ./server
-docker build -t alexanderttalvarez/multi-worker:latest -t alexanderttalvarez/multi-worker:$SHA -f ./worker/Dockerfile ./worker
-docker push alexanderttalvarez/multi-client:latest
-docker push alexanderttalvarez/multi-server:latest
-docker push alexanderttalvarez/multi-worker:latest
+docker build -t aelxan/multi-client:latest -t aelxan/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t aelxan/multi-server:latest -t aelxan/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t aelxan/multi-worker:latest -t aelxan/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+docker push aelxan/multi-client:latest
+docker push aelxan/multi-server:latest
+docker push aelxan/multi-worker:latest
 
-docker push alexanderttalvarez/multi-client:$SHA
-docker push alexanderttalvarez/multi-server:$SHA
-docker push alexanderttalvarez/multi-worker:$SHA
+docker push aelxan/multi-client:$SHA
+docker push aelxan/multi-server:$SHA
+docker push aelxan/multi-worker:$SHA
 
 kubectl apply -f k8s
-kubectl set image deployments/server-deployment server=alexanderttalvarez/multi-server:$SHA
-kubectl set image deployments/client-deployment client=alexanderttalvarez/multi-client:$SHA
-kubectl set image deployments/worker-deployment worker=alexanderttalvarez/multi-worker:$SHA
+kubectl set image deployments/server-deployment server=aelxan/multi-server:$SHA
+kubectl set image deployments/client-deployment client=aelxan/multi-client:$SHA
+kubectl set image deployments/worker-deployment worker=aelxan/multi-worker:$SHA
